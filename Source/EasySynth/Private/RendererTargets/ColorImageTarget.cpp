6955ef41fabfcb38bcbbd1ee2d1149dfbd05a381
Nikola Jovanovic <jovanovicn.96@gmail.com>
2021-11-12 17:59:02 +0100

    Add pp material assets

--- Source/EasySynth/Private/RendererTargets/ColorImageTarget.cpp
+++ Source/EasySynth/Private/RendererTargets/ColorImageTarget.cpp
@@ -2,14 +2,41 @@
 
 #include "RendererTargets/ColorImageTarget.h"
 
+#include "Camera/CameraComponent.h"
+
 #include "LevelSequence.h"
 
 
 bool FColorImageTarget::PrepareSequence(ULevelSequence* LevelSequence)
 {
-    TArray<UCameraComponent*> Cameras = GetCameras(LevelSequence);
-    UE_LOG(LogEasySynth, Error, TEXT("%s: Cameras num %d"), *FString(__FUNCTION__), Cameras.Num());
-    return true;
+    	// Get all camera components bound to the level sequence
+	TArray<UCameraComponent*> Cameras = GetCameras(LevelSequence);
+	if (Cameras.Num() == 0)
+	{
+		UE_LOG(LogEasySynth, Warning, TEXT("%s: No cameras bound to the level sequence found"), *FString(__FUNCTION__));
+		return false;
+	}
+
+	// Prepare the camera post process material
+	UMaterial* PostProcessMaterial = LoadPostProcessMatrial();
+	if (PostProcessMaterial == nullptr)
+	{
+		UE_LOG(LogEasySynth, Error, TEXT("%s: Could not load color post process material"), *FString(__FUNCTION__));
+		return false;
+	}
+
+	for (UCameraComponent* Camera : Cameras)
+	{
+		if (Camera == nullptr)
+		{
+			UE_LOG(LogEasySynth, Error, TEXT("%s: Found camera is null"), *FString(__FUNCTION__));
+			return false;
+		}
+		Camera->PostProcessSettings.WeightedBlendables.Array.Empty();
+		Camera->PostProcessSettings.WeightedBlendables.Array.Add(FWeightedBlendable(1.0f, PostProcessMaterial));
+	}
+
+	return true;
 }
 
 bool FColorImageTarget::FinalizeSequence(ULevelSequence* LevelSequence)
--- Source/EasySynth/Private/RendererTargets/DepthImageTarget.cpp
+++ Source/EasySynth/Private/RendererTargets/DepthImageTarget.cpp
@@ -2,14 +2,42 @@
 
 #include "RendererTargets/DepthImageTarget.h"
 
+#include "Camera/CameraComponent.h"
+
 #include "LevelSequence.h"
 
 
 bool FDepthImageTarget::PrepareSequence(ULevelSequence* LevelSequence)
 {
-    TArray<UCameraComponent*> Cameras = GetCameras(LevelSequence);
-    UE_LOG(LogEasySynth, Error, TEXT("%s: Cameras num %d"), *FString(__FUNCTION__), Cameras.Num());
-    return true;
+    // Get all camera components bound to the level sequence
+	TArray<UCameraComponent*> Cameras = GetCameras(LevelSequence);
+	if (Cameras.Num() == 0)
+	{
+		UE_LOG(LogEasySynth, Warning, TEXT("%s: No cameras bound to the level sequence found"), *FString(__FUNCTION__));
+		return false;
+	}
+
+	// Prepare the camera post process material
+	UMaterial* PostProcessMaterial = LoadPostProcessMatrial();
+	if (PostProcessMaterial == nullptr)
+	{
+		UE_LOG(LogEasySynth, Error, TEXT("%s: Could not load depth post process material"), *FString(__FUNCTION__));
+		return false;
+	}
+
+	for (UCameraComponent* Camera : Cameras)
+	{
+		if (Camera == nullptr)
+		{
+			UE_LOG(LogEasySynth, Error, TEXT("%s: Found camera is null"), *FString(__FUNCTION__));
+			return false;
+		}
+		Camera->PostProcessSettings.WeightedBlendables.Array.Empty();
+        // TODO: Utilize depth range parameter
+		Camera->PostProcessSettings.WeightedBlendables.Array.Add(FWeightedBlendable(1.0f, PostProcessMaterial));
+	}
+
+	return true;
 }
 
 bool FDepthImageTarget::FinalizeSequence(ULevelSequence* LevelSequence)
