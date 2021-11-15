401c1b246230694ad4e3617a11d532dc1332385b
NikolaJov96 <jovanovicn.96@gmail.com>
2021-11-15 14:32:04 +0100

    Abstract finalize rendering

--- Source/EasySynth/Public/RendererTargets/RendererTarget.h
+++ Source/EasySynth/Public/RendererTargets/RendererTarget.h
@@ -30,6 +30,9 @@ protected:
 	/** Extracts camera components used by the level sequence */
 	TArray<UCameraComponent*> GetCameras(ULevelSequence* LevelSequence);
 
+	/** Removes renderer target specific post-process materials */
+	bool ClearCameraPostProcess(ULevelSequence* LevelSequence);
+
 	/** Returns the path to the specific target post process material */
 	inline UMaterial* LoadPostProcessMatrial() const {
 		return LoadObject<UMaterial>(
